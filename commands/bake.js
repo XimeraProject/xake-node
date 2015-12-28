b9f1501bb66a06d9a839bee9e776add1f9a2fae8
Jim Fowler <fowler@math.osu.edu>
2015-12-27 22:33:20 -0500

    pdflatex and htlatex stages written

--- commands/bake.js
+++ commands/bake.js
@@ -5,6 +5,9 @@ var path = require('path');
 var recursive = require('recursive-readdir');
 var winston = null; // loaded from middleware
 var crypto = require('crypto');
+var progress = require('progress');
+var colors = require('colors');
+var compile = require('../lib/compile');
 
 var Command = require('ronin').Command;
 
@@ -190,8 +193,8 @@ function latexDependencies( filename, callback ) {
     });
 }
 
-/** @function texFilesToProcess examines all the files in the given directory (and its subdirectories) and calls callback with a list of files that require compilation */
-function texFilesToProcess( directory, callback ) {
+/** @function determineFilesToCompile examines all the files in the given directory (and its subdirectories) and calls callback with a list of files that require compilation */
+function determineFilesToCompile( directory, callback ) {
     async.waterfall([
 	// Fetch all the possible filenames
 	function(callback) {
@@ -291,21 +294,63 @@ function texFilesToProcess( directory, callback ) {
     });
 }
 
+function compileFiles( directory, filenames, jobLimit, callback ) {
+    var green = '#'.green;
+    var red = '.'.red;
+		
+    var bar = new progress('Compiling ' + '['.gray + ':bar' + ']'.gray + ' :percent (:etas remaining) ' + ':file'.magenta,
+			   { total: filenames.length,
+			     complete: green,
+			     width: 20,
+			     incomplete: red,
+			   });
+    
+    async.eachLimit( filenames, jobLimit, function(filename, callback) {
+	bar.tick(0, {file: path.relative( directory, filename )});
+	compile.compile( directory, filename, function(err) {
+	    if (err)
+		throw new Error(err);
+	    else {
+		bar.tick(1, {file: ''});
+		callback(null);
+	    }
+	});
+	
+    }, function(err) {
+	if (err)
+	    throw new Error(err);
+	else
+	    callback(null);
+    });
+}
 
 var BakeCommand = module.exports = Command.extend({
     use: ['winston', 'ximera-installed', 'find-repository-root'],
     
     desc: 'Convert the TeX input files to HTML suitable for Ximera',
 
-    run: function () {
+    options: {
+        jobs: {
+            type: 'integer',
+            alias: 'j',
+	    default: 1
+        }
+    },
+    
+    run: function (jobs) {
 	var global = this.global;
 	winston = global.winston;
+
+	if (jobs === true)
+	    jobs = 1;
 	
-	texFilesToProcess( global.repository, function(err, filenames) {
+	determineFilesToCompile( global.repository, function(err, filenames) {
 	    if (err)
 		throw new Error(err);
 	    else {
-		processFiles( filenames );
+		compileFiles( global.repository, filenames, jobs, function() {
+		    winston.info( "The xake is made." );
+		});
 	    }
 	});
 
