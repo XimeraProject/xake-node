aeaca1d72d72e8b55a8dc2aaa5e17caf8a33fafc
Jim Fowler <fowler@math.osu.edu>
2015-12-29 17:05:12 -0500

    Scaffold for publishing content to Ximera

---
+++ commands/publish.js
@@ -0,0 +1,18 @@
+var Command = require('ronin').Command;
+var winston = require('winston');
+
+var PublishCommand = module.exports = Command.extend({
+    use: ['winston', 'logged-in', 'find-repository-root'],
+    
+    desc: 'Publish the compiled content to Ximera',
+
+    options: {
+    },
+
+    run: function (key, secret) {
+	var global = this.global;
+	winston = global.winston;
+
+	winston.info("Publishing.");
+    }
+});
---
+++ middleware/logged-in.js
@@ -0,0 +1,20 @@
+var winston = require('winston');
+var credentials = require('../lib/credentials');
+var ximera = require('../lib/ximera-api');
+
+module.exports = function( next ) {
+    credentials.exists( function(loggedIn) {
+	if (loggedIn) {
+	    ximera.user( function(err, user) {
+		if (err) {
+		    winston.error( "Unable to connect to Ximera." );
+		    throw new Error(err);
+		} else {
+		    winston.info( "Logged in as " + user.name );
+		    next();
+		}
+	    });
+	} else
+	    throw new Error("You must be logged in.");
+    });    
+};
