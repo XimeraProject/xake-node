4aac14be7d4aa2f76eaac30021bce5b4d143ec54
Jim Fowler <fowler@math.osu.edu>
2015-12-29 17:23:36 -0500

    Separated out the file handling commands

--- commands/publish.js
+++ commands/publish.js
@@ -1,6 +1,8 @@
 var Command = require('ronin').Command;
 var winston = require('winston');
 
+
+
 var PublishCommand = module.exports = Command.extend({
     use: ['winston', 'logged-in', 'find-repository-root'],
     
